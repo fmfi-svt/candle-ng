f0d480ceac71c29a170bfa9890d823e8cc999323
Adam Zahradník <adam@zahradnik.xyz>
2023-09-11 19:08:22 +0200

    refactor: update init

--- candle/__init__.py
+++ candle/__init__.py
@@ -10,10 +10,6 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from candle.config import Config
 
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.wrappers import Response
-
-
 login_manager = LoginManager()  # keeps session data
 login_manager.login_view = 'auth.login'
 # login_manager.login_message_category = 'info'  # flash message category (not yet implemented)
@@ -28,13 +24,6 @@ def create_app(config_class=Config):
     app.url_map.strict_slashes = False
     app.config.from_object(config_class)
 
-    # Use middleware for 2016 path-prefix. Used for testing.  Source: https://dlukes.github.io/flask-wsgi-url-prefix.html#mwe
-    if app.config['ENV'] == "development":
-        app.wsgi_app = DispatcherMiddleware(
-            Response('Not Found', status=404),
-            {'/2016-2017-zima': app.wsgi_app}
-        )
-
     init_extensions(app)
     register_blueprints(app)
     return app
@@ -42,31 +31,33 @@ def create_app(config_class=Config):
 
 def register_blueprints(app):
     from candle.main.main import main
-    from candle.api.api import api
     from candle.auth.auth import auth
     from candle.timetable.timetable import timetable
     from candle.my_timetable.my_timetable import my_timetable
-    from candle.entities.student_group.student_group import student_group
-    from candle.entities.teacher.teacher import teacher
-    from candle.entities.subject.subject import subject
     from candle.panel.panel import panel
     from candle.search.search import search
     from candle.errors.errors import errors
     from candle.rooms.frontend import rooms
+    from candle.groups.frontend import groups
+    from candle.subjects.frontend import subjects
+    from candle.teachers.frontend import teachers
+    from candle.api import create_api
 
     app.register_blueprint(main)
-    app.register_blueprint(api)
     app.register_blueprint(auth)
     app.register_blueprint(timetable)
     app.register_blueprint(my_timetable)
-    app.register_blueprint(student_group)
-    app.register_blueprint(teacher)
-    app.register_blueprint(subject)
     app.register_blueprint(panel)
     app.register_blueprint(search)
     app.register_blueprint(errors)
 
+    api = create_api()
+    app.register_blueprint(api)
+
     app.register_blueprint(rooms)
+    app.register_blueprint(groups)
+    app.register_blueprint(subjects)
+    app.register_blueprint(teachers)
 
 
 def init_extensions(app):
